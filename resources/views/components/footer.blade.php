a57128a69a0d4f69839f4de73e019899fd759b98
PedroSpindola <pedrojf04@gmail.com>
2025-11-08 20:59:20 -0300

    feat: Criar componente footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+    <footer class="bg-dark text-white text-center py-3 mt-5">
+        <div class="container">
+            <p class="mb-0">
+                Gerenciador de Tarefas &copy; {{ date('Y') }}
+                <span class="d-none d-sm-inline mx-2">|</span>
+                <small class="text-muted d-block d-sm-inline">
+                    Desenvolvido por Pedro Spíndola
+                </small>
+            </p>
+        </div>
+    </footer>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,17 +18,7 @@
             </div>
         </div>
     </div>
-    <footer class="bg-dark text-white text-center py-3 mt-5">
-        <div class="container">
-            <p class="mb-0">
-                Gerenciador de Tarefas &copy; {{ date('Y') }}
-                <span class="d-none d-sm-inline mx-2">|</span>
-                <small class="text-muted d-block d-sm-inline">
-                    Desenvolvido por Pedro Spíndola
-                </small>
-            </p>
-        </div>
-    </footer>
+    @include('components.footer')
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
         integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
         crossorigin="anonymous"></script>
